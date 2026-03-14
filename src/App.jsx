243cd7e3d1ba0b07d1e400afc7a6801ce73d2d30
GowthamU7 <58745333+GowthamU7@users.noreply.github.com>
2026-03-14 00:19:19 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useMemo, useState } from "react";
 import axios from "axios";
 
-const API_BASE = "https://hirelensbe.onrender.com";
+const API_BASE = "https://hirelensbe.onrender.com/api";
 
 function cn(...classes) {
   return classes.filter(Boolean).join(" ");
